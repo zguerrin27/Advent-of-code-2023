235b2de58259008e35afac56468d405438f9e2a3
Zach Guerrin <zguerrin@hopper.com>
2023-12-02 20:49:34 -0500

    clean up

--- Day2/day2.js
+++ Day2/day2.js
@@ -61,28 +61,9 @@ const partOne = (games) => {
 };
 
 
-// console.log(partOne(games))
+console.log(partOne(games))
 
 // ============================ PART TWO ===============================
-
-
-
-/**
- * 
- * Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
- * Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
- * Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
- * Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
- * Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
- * 
- * loop through each section and keep track of each color in a map
- * if the current color exsits only keep the largest num
- * else enter the color and number
- * find product of each number
- * return product
- * keep a running total
- * 
- */
 
 const partTwo = (games) => {
 
