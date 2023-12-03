9f421584ecc8ef09a3fc38d01f99e3c0b7254593
Zach Guerrin <zguerrin@hopper.com>
2023-12-02 20:48:20 -0500

    day2

--- Day2/day2.js
+++ Day2/day2.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 
 const games = fs.readFileSync("./input.txt", 'utf-8').split('\n')
 
-const result = (games) => {
+// ============================ PART ONE ===============================
+
+const partOne = (games) => {
     const redMax = 12;
     const greenMax = 13;
     const blueMax = 14;
@@ -59,5 +61,63 @@ const result = (games) => {
 };
 
 
+// console.log(partOne(games))
 
-console.log(result(games))
+// ============================ PART TWO ===============================
+
+
+
+/**
+ * 
+ * Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+ * Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+ * Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+ * Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+ * Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+ * 
+ * loop through each section and keep track of each color in a map
+ * if the current color exsits only keep the largest num
+ * else enter the color and number
+ * find product of each number
+ * return product
+ * keep a running total
+ * 
+ */
+
+const partTwo = (games) => {
+
+    let total = 0
+    games.forEach((game, _) => {
+        total += processGame(game)
+    });
+    return total;
+    
+    function processGame(game){
+        const splitGame = game.split(":")
+        const gameNum = parseInt(splitGame.shift().match(/\d+/g)[0])
+        const gameSections = splitGame[0].split(';')
+
+        let dict = {
+            'red': 0,
+            'green': 0,
+            'blue': 0,
+        };
+
+        gameSections.forEach((section, _) => {
+            const segments = section.split(',')
+
+            segments.forEach((seg, _) => {
+                const count = parseInt(seg.match(/\d+/g)[0])
+                const color = seg.match(/red|green|blue/g)[0]
+
+                if(dict[color] < count){
+                    dict[color] = count
+                }
+            });
+        });
+
+        return Object.values(dict).reduce((a, b) => a * b);
+    };
+};
+
+console.log(partTwo(games))
